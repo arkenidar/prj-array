387432890672ba30acfeeb5483e501978657a48d
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-09-10 10:54:18 +0200

    funzionante

--- src/main/java/org/example/ArrayOperazioni.java
+++ src/main/java/org/example/ArrayOperazioni.java
@@ -1,2 +1,83 @@
-package org.example;public class ArrayOperazioni {
+package org.example;
+
+import java.util.Arrays;
+
+public class ArrayOperazioni {
+    private int[] array;
+
+    public ArrayOperazioni(int[] arrayIniziale) {
+        array = arrayIniziale;
+    }
+
+    public void aggiungiFine(int x) {
+        // accresci
+        int[] array2 = new int[array.length + 1];
+        // conserva
+        int[] iniziale = array;
+        // ricopia
+        array = array2; // accresciuto
+        ricopia(iniziale);
+        // aggiungi
+        int indiceFine = array.length - 1;
+        array[indiceFine] = x;
+    }
+
+    public void ricopia(int[] array) {
+        ricopia(array, 0);
+    }
+
+    public void ricopia(int[] array2, int indiceInizio) {
+        for (int i = 0; i < array2.length; i++) {
+            int elemento = array2[i];
+            array[i + indiceInizio] = elemento; // copia elemento
+        }
+    }
+
+    public void aggiungiInizio(int x) {
+        // accresci
+        int[] array2 = new int[array.length + 1];
+        // conserva
+        int[] iniziale = array;
+        // ricopia
+        array = array2; // accresciuto
+        // inizia da dopo, dopo l'inizio
+        ricopia(iniziale, 1);
+        // aggiungi
+        int indiceInizio = 0;
+        array[indiceInizio] = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayOperazioni{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
+
+    public void aggiungiFine(int[] arrayArg) {
+        int lunghezzaIniz = array.length;
+        // accresci
+        int[] array2 = new int[array.length + arrayArg.length];
+        // conserva
+        int[] iniziale = array;
+        // ricopia
+        array = array2; // accresciuto
+        ricopia(iniziale);
+        // aggiungi
+        ricopia(arrayArg, lunghezzaIniz);
+    }
+
+    public void aggiungiInizio(int[] arrayArg) {
+        // accresci
+        int[] arrayN = new int[array.length + arrayArg.length];
+
+        int[] iniziale = array; // conserva
+
+        // ricopia
+        array = arrayN;
+        ricopia(iniziale, arrayArg.length);
+
+        // aggiungi
+        ricopia(arrayArg, 0);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
-package org.example;public class Main {
+package org.example;
+
+import java.io.PrintStream;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PrintStream out = System.out;
+
+        out.println("Array!");
+
+        int[] serie1 = {1, 2, 3, 4};
+
+        ArrayOperazioni array = new ArrayOperazioni(serie1);
+
+        int[] serie2 = {5, 6, 7, 8};
+        array.aggiungiFine(10);
+        array.aggiungiInizio(-10);
+
+        out.println(array);
+
+        array.aggiungiInizio(serie2); out.println(array);
+        array.aggiungiFine(new int[]{11,22,33}); out.println(array);
     }
 }
